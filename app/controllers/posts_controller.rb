8306a1d0c8afa0dbb403bbf1bb60d994de449b89
Miguel <miaortizma@unal.edu.co>
2019-07-09 21:18:01 -0500

    working, you may need to change wkhtml path

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,60 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  # GET /posts
+  # GET /posts.json
+  def index
+    @posts = Post.all.paginate(page: params[:page], per_page: 3).order('created_at DESC')
+  end
+
+  # GET /posts/1
+  # GET /posts/1.json
+  def show
+    @post = Post.find(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf{render pdf: "pdf_name"} # Excluding ".pdf" extension
+    end
+  end
+
+  # GET /posts/new
+  def new
+    @post = Post.new
+  end
+
+  # GET /posts/1/edit
+  def edit
+  end
+
+  # POST /posts
+  # POST /posts.json
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /posts/1
+  # PATCH/PUT /posts/1.json
+  def update
+    if @post.update_attributes(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def post_params
+      params.require(:post).permit(:title, :body, :image)
+    end
+end
